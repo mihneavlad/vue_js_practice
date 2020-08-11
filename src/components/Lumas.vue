13d111b55336ca1f5a3c010b103fb1098198813c
Mihnea Vlad <mihnea.vlad@verticalmedia.com>
2020-08-11 15:06:22 +0200

    obtained title for each individual article

--- src/components/Lumas.vue
+++ src/components/Lumas.vue
@@ -8,7 +8,8 @@
           id="carousel-1"
           v-model="slide"
           :interval="10000"
-          controls="true"
+          controls
+          indicators
           img-width="520"
           img-height="250"
           style="text-shadow: 1px 1px 2px #333;"
@@ -18,7 +19,6 @@
           <!-- Text slides with image -->
           <b-carousel-slide
             v-for="i in 5" :key="i"
-            caption="First slide"
             img-src="https://picsum.photos/520/250/?image=52"
           ></b-carousel-slide>
         </b-carousel>
@@ -70,70 +70,28 @@ fetch(proxyUrl + url1)
     });
 
     items.forEach((el, index) => {
-      // if (index > 2) {
-      //   return false;
-      // }
       if (index > 4) {
         return;
       }
 
       let imageLink = `${el.querySelector("enclosure")['attributes'].getNamedItem('url').value}`;
       let title = `${el.querySelector("title").innerHTML}`;
-
-
-      // console.log(imageLink);
-      // console.log('[aria-posinset="' + `${index}` + '"]');
-      // console.log('[data-glide-index="' + `${index}` + '"]');
-      // let parentSlide = document.querySelectorAll('[data-glide-index="' + `${index}` + '"]')[0];
-
-      // console.log('miorlau');
-      // console.log('[data-slide-index="0"]');
-      // console.log('[data-slide-index="' + `${index}` + '"]');
-      //
-      // let parentSlide = document.querySelector('[data-slide-index="0"]');
-      // let parentSlide0 = document.querySelector('[data-slide-index="5"]');
       let parentSlide = document.querySelector('[data-slide-index="' + `${index}` + '"]');
       console.log(parentSlide);
       // console.log(parentSlide.firstElementChild.innerHTML);
       let img = parentSlide.firstElementChild;
-      img.src = imageLink
       console.log(img);
-      // let parentSlide = document.querySelector('.carousel-inner');
-      // console.log('bar');
+      img.src = imageLink;
 
-      // let tiles = document.querySelectorAll('.carousel-item');
-      // tiles.forEach(tile => {
-      //   console.log(imageLink);
-      //   console.log(imgLink);
-      // })
-
-      // var tileContainer = parentSlide.firstElementChild;
-      // console.log(tileContainer);
-      // console.log('foo');
-      // console.log(imgLink);
-      // console.log('miau');
-      // imgLink.src = imageLink;
-      // var caption = document.querySelector('.carousel-caption').firstElementChild;
-      // caption.innerHTML = title;
-      // console.log('miau');
-      // let nextSlide = tileContainer.firstElementSibling;
-      // console.log(nextSlide);
-      // let nextSlide = tileContainer.nextElementSibling;
-
-
-
-      // html =
-      //   `
-
-      //     <img src="${imageLink}">
-      //     <h3>
-      //       <a href="${el.querySelector("link").innerHTML}" target="_blank" rel="noopener" class="glidelink">
-      //         ${title}
-      //       </a>
-      //     <p>foo</p>
-      //     </h3>
-      // `;
-      // parentSlide.insertAdjacentHTML("beforeend", html);
+      let html =
+      `
+        <h3 class="article-title">
+        <a href="${el.querySelector("link").innerHTML}" target="_blank" rel="noopener">
+                 ${title}
+        </a>
+        </h3>
+       `;
+       parentSlide.insertAdjacentHTML("beforeend", html);
     });
   });
 
@@ -144,7 +102,6 @@ export default {
     return {
       slide: 0,
       sliding: null,
-      // imageLink: `${el.querySelector("enclosure")['attributes'].getNamedItem('url').value}`
     }
   },
   methods: {
@@ -161,11 +118,6 @@ export default {
   <!-- Add "scoped" attribute to limit
   CSS to this component only -->
 <style lang="scss" scoped>
-  // SIMPLE GRID - SASS/SCSS
-
-  // @import url(https://fonts.googleapis.com/css?family=Lato:400,300,300italic,400italic,700,700italic);
-  // @import "node_modules/@glidejs/glide/src/assets/sass/glide.core";
-  // @import "node_modules/@glidejs/glide/src/assets/sass/glide.theme";
   @import 'node_modules/bootstrap/scss/bootstrap';
   @import 'node_modules/bootstrap-vue/src/index.scss';
 
